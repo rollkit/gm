aff1d5484c79af7df47b7a1f172271fd92a80e30
Smuu <18609909+Smuu@users.noreply.github.com>
2023-04-13 10:30:41 +0200

    feat: simplify setup
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,7 +9,7 @@ echo "NAMESPACE_ID: $NAMESPACE_ID"
 # to allow users to interact with Celestia's nodes by querying
 # the node's state and broadcasting transactions on the Celestia
 # network. The default port is 26657.
-DA_BLOCK_HEIGHT=$(curl https://rpc-blockspacerace.pops.one/block | jq -r '.result.block.header.height')
+DA_BLOCK_HEIGHT=$(curl $RPC | jq -r '.result.block.header.height')
 echo "DA_BLOCK_HEIGHT: $DA_BLOCK_HEIGHT"
 
 if [ -z "$DA_BLOCK_HEIGHT" ]; then
@@ -17,4 +17,4 @@ if [ -z "$DA_BLOCK_HEIGHT" ]; then
     exit 1
 fi
 
-exec gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://celestia-light-node:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT
+exec gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://celestia:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' --rollkit.namespace_id $NAMESPACE_ID --rollkit.da_start_height $DA_BLOCK_HEIGHT
